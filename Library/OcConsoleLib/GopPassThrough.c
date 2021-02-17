efe91f3030441ba6ad73b5e8fe4267286ed4ae45
PMheart <17109513+PMheart@users.noreply.github.com>
2021-02-17 11:34:31 +0800

    OcConsoleLib: Implement Query, Set, and Blt methods for OcProvideGopPassThrough

--- Library/OcConsoleLib/GopPassThrough.c
+++ Library/OcConsoleLib/GopPassThrough.c
@@ -22,17 +22,30 @@
 STATIC
 EFI_STATUS
 EFIAPI
-OcGopDrawGetMode (
-  IN  EFI_GRAPHICS_OUTPUT_PROTOCOL  *This,
-  OUT UINT32                        *HorizontalResolution,
-  OUT UINT32                        *VerticalResolution,
-  OUT UINT32                        *ColorDepth,
-  OUT UINT32                        *RefreshRate
+OcGopDrawQueryMode (
+  IN  EFI_GRAPHICS_OUTPUT_PROTOCOL          *This,
+  IN  UINT32                                ModeNumber,
+  OUT UINTN                                 *SizeOfInfo,
+  OUT EFI_GRAPHICS_OUTPUT_MODE_INFORMATION  **Info
   )
 {
-  //
-  // TODO
-  //
+  if (ModeNumber != 0) {
+    DEBUG ((DEBUG_VERBOSE, "OCC: OcGopDrawQueryMode invalid ModeNumber - %u\n", ModeNumber));
+    return EFI_INVALID_PARAMETER;
+  }
+
+  if (SizeOfInfo == NULL || Info == NULL) {
+    DEBUG ((DEBUG_VERBOSE, "OCC: OcGopDrawQueryMode got invalid parameter SizeOfInfo or Info!\n"));
+    return EFI_INVALID_PARAMETER;
+  }
+
+  *SizeOfInfo = This->Mode->SizeOfInfo;
+  *Info       = AllocateCopyPool (This->Mode->SizeOfInfo, This->Mode->Info);
+  if (*Info == NULL) {
+    DEBUG ((DEBUG_VERBOSE, "OCC: OcGopDrawQueryMode failed to allocate memory for GOP Info!\n"));
+    return EFI_DEVICE_ERROR;
+  }
+
   return EFI_SUCCESS;
 }
 
@@ -41,14 +54,16 @@ EFI_STATUS
 EFIAPI
 OcGopDrawSetMode (
   IN  EFI_GRAPHICS_OUTPUT_PROTOCOL  *This,
-  IN  UINT32                        HorizontalResolution,
-  IN  UINT32                        VerticalResolution,
-  IN  UINT32                        ColorDepth,
-  IN  UINT32                        RefreshRate
+  IN  UINT32                        ModeNumber
   )
 {
+  if (ModeNumber != 0) {
+    DEBUG ((DEBUG_VERBOSE, "OCC: OcGopDrawSetMode unsupported ModeNumber - %u\n", ModeNumber));
+    return EFI_UNSUPPORTED;
+  }
+
   //
-  // TODO
+  // Assuming 0 is the only mode that is accepted, which is already set.
   //
   return EFI_SUCCESS;
 }
@@ -58,7 +73,7 @@ EFI_STATUS
 EFIAPI
 OcGopDrawBlt (
   IN  EFI_GRAPHICS_OUTPUT_PROTOCOL            *This,
-  IN  EFI_GRAPHICS_OUTPUT_BLT_PIXEL           *BltBuffer OPTIONAL,
+  IN  EFI_GRAPHICS_OUTPUT_BLT_PIXEL           *BltBuffer    OPTIONAL,
   IN  EFI_GRAPHICS_OUTPUT_BLT_OPERATION       BltOperation,
   IN  UINTN                                   SourceX,
   IN  UINTN                                   SourceY,
@@ -66,13 +81,25 @@ OcGopDrawBlt (
   IN  UINTN                                   DestinationY,
   IN  UINTN                                   Width,
   IN  UINTN                                   Height,
-  IN  UINTN                                   Delta      OPTIONAL
+  IN  UINTN                                   Delta         OPTIONAL
   )
 {
-  //
-  // TODO
-  //
-  return EFI_SUCCESS;
+  OC_GOP_PROTOCOL  *OcGopDraw;
+
+  OcGopDraw = BASE_CR (This, OC_GOP_PROTOCOL, GraphicsOutput);
+
+  return OcGopDraw->Uga->Blt (
+    OcGopDraw->Uga,
+    (EFI_UGA_PIXEL *) BltBuffer,
+    (EFI_UGA_BLT_OPERATION) BltOperation,
+    SourceX,
+    SourceY,
+    DestinationX,
+    DestinationY,
+    Width,
+    Height,
+    Delta
+    );
 }
 
 EFI_STATUS
@@ -85,8 +112,8 @@ OcProvideGopPassThrough (
   //
   EFI_STATUS  Status;
 
-  Status = OcGopDrawGetMode (NULL, NULL, NULL, NULL, NULL);
-  Status = OcGopDrawSetMode (NULL, 0, 0, 0, 0);
+  Status = OcGopDrawQueryMode (NULL, 0, NULL, NULL);
+  Status = OcGopDrawSetMode (NULL, 0);
   Status = OcGopDrawBlt (NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0);
 
   return Status;
